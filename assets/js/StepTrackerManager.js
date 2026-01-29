84cc6c388980711aa83591bf7010da16388c6eca
paulwasthere <132862961+ZBZFirst@users.noreply.github.com>
2026-01-29 07:07:23 -0800

    Update StepTrackerManager.js

--- assets/js/StepTrackerManager.js
+++ assets/js/StepTrackerManager.js
@@ -383,7 +383,7 @@ export class StepTrackerManager {
         }
         
         return this;
-    }}
+    }
     
     getCurrentGuess() {
         return parseInt(this.get('current-guess')) || 0;
